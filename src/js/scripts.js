f3ba933554f058911186c3e94ddd68eea219c8f6
unknown <vitaliyshvedq@gmail.com>
2023-12-03 19:41:59 +0200

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,44 @@
+var display = document.getElementById('display');
+var buttonsContainer = document.getElementById('buttons-container');
+
+buttonsContainer.addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        if (event.target.classList.contains('buttons')) {
+            display.value += event.target.textContent;
+           
+        // if (event.target.classList.contains('buttons')) {
+        //     // Проверка первого символа
+        //     if (display.value === '' && event.target.textContent !== '-' && isNaN(event.target.textContent)) {
+        //         // Если строка ввода пуста и введенный символ не является минусом или числом, игнорируем его
+        //         return;
+        //     }
+        //     // Проверка, является ли содержимое кнопки числом
+        //     if (!isNaN(event.target.textContent)) {
+        //         display.value += event.target.textContent;
+        //     } else if (event.target.textContent === '-' && display.value === '') {
+        //         // Добавляем минус только если строка ввода пуста
+        //         display.value += event.target.textContent;
+        //     }    
+            
+            
+        } else if (event.target.classList.contains('cleardisplay')) {
+            display.value = '';
+        } else if (event.target.classList.contains('equals')) {
+            try {
+                display.value = operation(display.value);
+            } catch (error) {
+                display.value = 'Помилка';
+            }
+        }
+    }
+});
+
+
+function operation(mathString) {
+    return new Function('return ' + mathString)();
+}
+
+// через eval
+// function operation(expression) {
+//     return eval(expression);
+// }
